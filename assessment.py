fb60923166e971e76c571e2a606972f78e41b332
llonne <lindsey.lonne@gmail.com>
2017-01-15 07:42:21 +0000

    Completed section one.

--- assessment.py
+++ assessment.py
@@ -3,17 +3,35 @@ Part 1: Discussion
 
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
+   - Abstraction: Allows you to obscure all but the relevant data about an object
+        to reduce complexity and repetition, and increase efficiency.
+   - Encapsulation: You don't need to know/understand details in the code to use it.
+   - Polymorphism: You can use the same simple method for accessing differing
+        (possibly more complex) underlying data. Often using class inheritance.
 
 2. What is a class?
+    - An object that lets you structure your software to add consistency
+    to your programs so that they can be used in a cleaner way.
 
 3. What is an instance attribute?
+    - Instance attributes only apply to the specific instance of a class.
 
 4. What is a method?
+    - A method is a function which is a member of a class.
 
 5. What is an instance in object orientation?
+    - A specific realization of any class. You must instantiate a class to use its instance.
 
 6. How is a class attribute different than an instance attribute?
+    - Class attributes are same for all instances of class.
+    - Instance attributes only apply to the individual instance of a class.
+
    Give an example of when you might use each.
+
+class(Cat):
+    sound = purr # class attribute
+fluffy = Cat()
+fluffy.name = "fluffy" # instance attribute
 
 
 """
